db7616959de7fb3b29ac8a8da3727c108cf604fb
Pankaj Kumar <pankajkumar199922@gmail.com>
2026-03-16 11:43:29 +0530

    update swap button

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -6,11 +6,15 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((response)=>response.json())
         .then((response)=> setData(response[currency]))
+        
     },[currency])
-       
+    
     return data
-
+        
 }
+   
+       
+
 
 export default useCurrencyInfo
 
